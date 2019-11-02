c6842ceb847d7655db2c5dc1b58bf5711ec52d9f
Arjun Subedi <arjunsubedi@live.com>
2019-11-02 14:18:13 +0545

    Added Invoice Config and Invoice Config Detail

--- PeaceInternational.Infrastructure/EntityConfiguration/InvoiceConfig.cs
+++ PeaceInternational.Infrastructure/EntityConfiguration/InvoiceConfig.cs
@@ -1,4 +1,6 @@
-﻿using PeaceInternational.Core.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaceInternational.Core.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace PeaceInternational.Infrastructure.EntityConfiguration
 {
-    public class InvoiceConfig: BaseEntityConfig<Invoice>
+    public class InvoiceConfig: BaseEntityConfig<Invoice>, IEntityTypeConfiguration<Invoice>
     {
+        public override void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.InvoiceNo)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.FileCodeNo)
+                .IsRequired();
+
+            builder.Property(p => p.RefrenceNo)
+               .HasMaxLength(255)
+               .IsRequired();
+
+            builder.Property(p => p.Dr)
+                .HasMaxLength(255)
+               .IsRequired();
+
+            builder.Property(p => p.AgentName)
+                .HasMaxLength(255)
+               .IsRequired();
+
+            builder.Property(p => p.ClientName)
+                .HasMaxLength(255)
+               .IsRequired();
+
+            builder.Property(p => p.PAX)                
+               .IsRequired();
+
+            builder.Property(p => p.Currency)
+                .HasMaxLength(255)
+               .IsRequired();
+        }
     }
 }
---
+++ PeaceInternational.Infrastructure/EntityConfiguration/InvoiceDetailConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeaceInternational.Core.Entity;
+
+namespace PeaceInternational.Infrastructure.EntityConfiguration
+{
+    public class InvoiceDetailConfig: BaseEntityConfig<InvoiceDetail>, IEntityTypeConfiguration<InvoiceDetail>
+    {
+        public override void Configure(EntityTypeBuilder<InvoiceDetail> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.Particulars)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(p => p.Amount)
+                .HasColumnType("float");
+        }
+    }
+}
