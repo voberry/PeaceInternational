99656761bf450a1ec765ce64ada955b3f61667b3
Arjun Subedi <arjunsubedi@live.com>
2019-12-09 20:43:25 +0545

    Futher changes in invoice and service voucher

--- PeaceInternational/wwwroot/lib/customHelper.js
+++ PeaceInternational/wwwroot/lib/customHelper.js
@@ -17,7 +17,7 @@ Handlebars.registerHelper("inWords", function (num, options) {
     str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'Lakh ' : '';
     str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'Thousand ' : '';
     str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'Hundred ' : '';  
-    str += (n[5] != 0) ? (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'Only' : '' ;
+    str += (n[5] != 0) ? (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'Only' : '';    
     return str;
 });
 
